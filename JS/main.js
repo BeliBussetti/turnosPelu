9ba94916f05c32530a8fc04f8778c40e9281cd72
BeliBussetti <belisariobussetti@gmail.com>
2024-06-21 18:59:18 -0300

    fix fecha

--- JS/main.js
+++ JS/main.js
@@ -5,32 +5,34 @@ document.addEventListener('DOMContentLoaded', function () {
     dias.forEach(dia => {
         const li = document.createElement('li');
         li.innerHTML = `
-            <span class="dia">${capitalizarPrimeraLetra(dia.dia)}, ${dia.fecha}</span>
-            <button class="btn" onclick="verTurnos('${dia.fecha}')">Ver Turnos</button>
+            <span class="dia">${capitalizarPrimeraLetra(dia.dia)} ${dia.fecha}</span>
+            <button class="btn" onclick="verTurnos('${dia.fechaCompleta}')">Ver Turnos</button>
         `;
         listaDias.appendChild(li);
     });
 });
 
 function verTurnos(fecha) {
-    // Aquí redireccionas a la página de turnos.html con la fecha como parámetro
     window.location.href = `pages/turnos.html?fecha=${encodeURIComponent(fecha)}`;
 }
 
 function generarDiasDeSemana() {
     const dias = [];
     const today = new Date();
-    const options = { weekday: 'long', day: 'numeric', month: 'short' };
+    const optionsDia = { weekday: 'long' };
+    const optionsFecha = { day: 'numeric', month: 'short' };
     let dayCount = 0;
 
     while (dias.length < 7) {
         const nextDay = new Date(today);
         nextDay.setDate(today.getDate() + dayCount);
         if (nextDay.getDay() !== 0) {
-            const diaString = nextDay.toLocaleDateString('es-ES', options);
+            const diaString = nextDay.toLocaleDateString('es-ES', optionsDia);
+            const fechaString = nextDay.toLocaleDateString('es-ES', optionsFecha).replace(/de /g, '');
             dias.push({
                 dia: diaString.split(',')[0],
-                fecha: nextDay.toISOString().split('T')[0] // Formato ISO para compatibilidad
+                fecha: fechaString,
+                fechaCompleta: nextDay.toISOString().split('T')[0] // Formato ISO para compatibilidad
             });
         }
         dayCount++;
